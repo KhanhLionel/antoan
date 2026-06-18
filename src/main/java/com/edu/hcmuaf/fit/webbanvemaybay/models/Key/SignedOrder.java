bdb49582c3619d44887fa1f8f3b9660df8362719
HoangBaKhanh <23130151@st.hcmuaf.edu.vn>
2026-06-18 17:33:20 +0700

    Cập nhật model khóa, model chữ kỹ và rsahelper cho khớp với db mới

--- src/main/java/com/edu/hcmuaf/fit/webbanvemaybay/models/Key/SignedOrder.java
+++ src/main/java/com/edu/hcmuaf/fit/webbanvemaybay/models/Key/SignedOrder.java
@@ -6,23 +6,94 @@ public class SignedOrder {
     private int id;
     private int idDonHang;
     private int idUserKey;
+
+    private String payloadHash;
     private String signature;
-    private String thongTinGoc;
+    private String signatureAlgorithm;
+    private String verifyStatus;
+    private boolean current;
+
     private Timestamp ngayKy;
 
     public SignedOrder() {}
 
-    // Getter và Setter
-    public int getId() { return id; }
-    public void setId(int id) { this.id = id; }
-    public int getIdDonHang() { return idDonHang; }
-    public void setIdDonHang(int idDonHang) { this.idDonHang = idDonHang; }
-    public int getIdUserKey() { return idUserKey; }
-    public void setIdUserKey(int idUserKey) { this.idUserKey = idUserKey; }
-    public String getSignature() { return signature; }
-    public void setSignature(String signature) { this.signature = signature; }
-    public String getThongTinGoc() { return thongTinGoc; }
-    public void setThongTinGoc(String thongTinGoc) { this.thongTinGoc = thongTinGoc; }
-    public Timestamp getNgayKy() { return ngayKy; }
-    public void setNgayKy(Timestamp ngayKy) { this.ngayKy = ngayKy; }
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+
+    public int getIdDonHang() {
+        return idDonHang;
+    }
+
+    public void setIdDonHang(int idDonHang) {
+        this.idDonHang = idDonHang;
+    }
+
+
+    public int getIdUserKey() {
+        return idUserKey;
+    }
+
+    public void setIdUserKey(int idUserKey) {
+        this.idUserKey = idUserKey;
+    }
+
+
+    public String getPayloadHash() {
+        return payloadHash;
+    }
+
+    public void setPayloadHash(String payloadHash) {
+        this.payloadHash = payloadHash;
+    }
+
+
+    public String getSignature() {
+        return signature;
+    }
+
+    public void setSignature(String signature) {
+        this.signature = signature;
+    }
+
+
+    public String getSignatureAlgorithm() {
+        return signatureAlgorithm;
+    }
+
+    public void setSignatureAlgorithm(String signatureAlgorithm) {
+        this.signatureAlgorithm = signatureAlgorithm;
+    }
+
+
+    public String getVerifyStatus() {
+        return verifyStatus;
+    }
+
+    public void setVerifyStatus(String verifyStatus) {
+        this.verifyStatus = verifyStatus;
+    }
+
+
+    public boolean isCurrent() {
+        return current;
+    }
+
+    public void setCurrent(boolean current) {
+        this.current = current;
+    }
+
+
+    public Timestamp getNgayKy() {
+        return ngayKy;
+    }
+
+    public void setNgayKy(Timestamp ngayKy) {
+        this.ngayKy = ngayKy;
+    }
 }
